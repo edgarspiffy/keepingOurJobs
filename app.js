3eb6d25d7350130548893a9817cd19fa8de52cfa
Edgar <elimon@pos360.com>
2021-09-09 13:50:46 -0700

    added more logic for string comparison, refactored code, everything is working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import ExcelJS from 'exceljs';
+import ExcelJS     from 'exceljs';
 import excelToJson from 'convert-excel-to-json';
 
 const excelData = excelToJson({
@@ -15,7 +15,7 @@ const excelData = excelToJson({
     }
 })['Sheet1'];
 
-const newData =[];
+const matchResults =[];
 
 const formatString = {
     cleanString:function(string){
@@ -24,23 +24,18 @@ const formatString = {
         string = string.replace( /\(/g, "");
         string = string.replace( /\)/g, "");
         string = string.replace(/\u0301|\u00e9/g,'e'); // é to e 
-        string = string.replace(/\u00E4/g,'a'); // ä to a 
-        string = string.replace(/\u00F1/g,'n'); // ñ to n
-        string = string.replace(/snapples/g,'snapple');
+        string = string.replace(/\u00F3|\u00F2/g,'e'); // ó to o 
+        string = string.replace(/\u00E4/g,'a');        // ä to a 
+        string = string.replace(/\u00F1/g,'n');        // ñ to n
+        string = string.replace(/-/g,' ');             // remove -
+        string = string.replace(/'/g,'');              // remove '
         string = string.replace(/\syear/g,' yr');
-        string = string.replace(/\s+no\./g,' no');
-        string = string.replace(/a\s&\sj/g,'e&j');
-        string = string.replace(/j\s&\sb/g,'j&b');
-        string = string.replace(/-/g,' '); // remove -
-        string = string.replace(/a\s&\sw/g,'a&w'); // remove -
-
-
         //outlier(s)
         string = string.replace('355ml','12oz');
         //regexp
         const lookForOz    = RegExp("(\\s+[0-9]+\\s+oz)|(\\s+[0-9]+oz)|(\\s+[0-9]+\\.[0-9]+oz)|(\\s+[0-9]+\\.[0-9]+\\s+oz)");
         const lookForMl    = RegExp("(\\s+[0-9]+\\s+ml)|(\\s+[0-9]+ml)|(\\s+[0-9]+\\.[0-9]+ml)|(\\s+[0-9]+\\.[0-9]+\\s+ml)");
-        const lookForLiter = RegExp("(\\s+[0-9]+\\s+l)|(\\s+[0-9]+ml)|(\\s+[0-9]+\\.[0-9]+l)|(\\s+[0-9]+\\.[0-9]+\\s+l)");
+        const lookForLiter = RegExp("(\\s+[0-9]+\\s+l)|(\\s+[0-9]+l)|(\\s+[0-9]+\\.[0-9]+l)|(\\s+[0-9]+\\.[0-9]+\\s+l)");
 
         if(lookForOz.test(string)){
             string = this.removeOz(string);
@@ -71,7 +66,6 @@ const formatString = {
             string = string.replace(numWithOz,numOnly);
             return string;
         }
-        
         if(lookForOzAndDecimal.test(string)){   
             let numWithOz = lookForOzAndDecimal.exec(string)[0];
             let numOnly = numWithOz.slice(0,-2);
@@ -112,7 +106,7 @@ const formatString = {
         }
     },
     removeLiter:function(string){
-        const lookForLiter = RegExp("(\\s+[0-9]+\\s+l)|(\\s+[0-9]+ml)|(\\s+[0-9]+\\.[0-9]+l)|(\\s+[0-9]+\\.[0-9]+\\s+l)");                
+        const lookForLiter = RegExp("(\\s+[0-9]+\\s+l)|(\\s+[0-9]+l)|(\\s+[0-9]+\\.[0-9]+l)|(\\s+[0-9]+\\.[0-9]+\\s+l)");                  
 
             let numWithLiter = lookForLiter.exec(string)[0];
             let numOnly = numWithLiter.slice(0,-1);
@@ -129,149 +123,109 @@ const formatString = {
         return string;
     },
     wordsToRemove:[ //plural needs to come before singular words so bottles needs to come before bottle
-        "bottle","bottles",
+        "bottles","bottle",
         "cans","can",
         "'s",'\xAE',
-        "ct",
+        "ct","bags","bag",
+        "boxes","box","plastic",
+        "count"
     ],
 };
 
 
 
 const calculateStringMatch = {
-    originalStringMatch:function(excelData){
+    getMatchResults:function(excelData){
         for(let i = 0; i < excelData.length;i++){
-            let productA = array1[i].split(" ");
-            let productB = array2[i].split(" ");
-    
-            let matchCount = 0;
-    
-            for(let j = 0; j < productA.length;j++){
-                for(let m = 0; m < productB.length; m++){
-                    if(productA[j] === productB[m]){
-                        matchCount++;
-                    }
-                }
-            }
-            const accuracy = (matchCount/productA.length)*100;
-            console.log(`${accuracy}% || ${productA} || ${productB}`)
+
+            let productA      = excelData[i]['ucName'];
+            let productB      = excelData[i]['drizlyName'];
+            let productAClean = formatString.cleanString(excelData[i]['ucName']);
+            let productBClean = formatString.cleanString(excelData[i]['drizlyName']);
+
+            productA      = productA.split(" ");
+            productB      = productB.split(" ");
+            productAClean = productAClean.split(" ");
+            productBClean = productBClean.split(" ");
+
+            let stringMatchOriginalAtoB = this.getMatchPercentage(productA,productB);
+            let stringMatchOriginalBtoA = this.getMatchPercentage(productB,productA);
+            let stringMatchCleanAtoB    = this.getMatchPercentage(productAClean,productBClean);
+            let stringMatchCleanBtoA    = this.getMatchPercentage(productBClean,productAClean);
+
+            let resultForOriginalComparison = (stringMatchOriginalAtoB + stringMatchOriginalBtoA)/2
+            let resultForCleanComparison    = (stringMatchCleanAtoB + stringMatchCleanBtoA)/2
+
+            resultForOriginalComparison = parseFloat(resultForOriginalComparison.toFixed(2));
+            resultForCleanComparison    = parseFloat(resultForCleanComparison.toFixed(2));
+
+
+            productAClean = productAClean.join(" ");
+            productBClean = productBClean.join(" ");
+
+            matchResults.push(excelData[i] = {
+                'itemNum'                 :excelData[i]['itemNum'],
+                'UPC'                     :excelData[i]['UPC'],
+                'drizlyName'              :excelData[i]['drizlyName'],
+                'ucName'                  :excelData[i]['ucName'],
+                'category'                :excelData[i]['category'],
+                'ucNameClean'             :productAClean,
+                'drizlyNameClean'         :productBClean,
+                'originalPercentageMatch' :resultForOriginalComparison,
+                'cleanPercentageMatch'    :resultForCleanComparison
+            });
+ 
+        
         }
     },
-    cleanStringMatch:function(excelData){
-        for(let i = 0; i < excelData.length;i++){
-            let productA = formatString.cleanString(excelData[i]['ucName']);
-            let productB = formatString.cleanString(excelData[i]['drizlyName']);
-            productA = productA.split(" ");
-            productB = productB.split(" ");
-
-            const getMatchPercentage = function(productA,productB){
-                let matchwingWordsCountAtoB = 0;
-                let arraySubtractorAtoB = 0; //when a word gets conca and a match returns we need to subtract it from word count
-                
-                for(let j = 0; j < productA.length;j++){
-                    for(let m = 0; m < productB.length; m++){
-                        if(productA[j] === productB[m]){
-                            matchwingWordsCountAtoB++;
-                            break;
-                        }
-                        if(`${productA[j]}.` === productB[m]){
-                            matchwingWordsCountAtoB++;
-                            break;
-                        }
-                        if(`${productA[j]}${productA[j+1]}` === productB[m]){
-                            matchwingWordsCountAtoB++;
-                            arraySubtractorAtoB++
-                            break;
-                        }
-                    }
+    getMatchPercentage:function(productA,productB){
+        let matchwingWordsCountAtoB = 0;
+        let arraySubtractorAtoB = 0; //when a word gets concatenated and a match returns we need to subtract it from word count
+        for(let j = 0; j < productA.length;j++){
+            for(let m = 0; m < productB.length; m++){
+                if(productA[j] === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    break;
                 }
-                let accuracyAtoB = (matchwingWordsCountAtoB/(productA.length - arraySubtractorAtoB))*100;
-                return accuracyAtoB;
+                if(`${productA[j]}.` === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    break;
+                }
+                if(`${productA[j]}s` === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    break;
+                }
+                if(productA[j].slice(0, -1) === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    break;
+                }
+                if(`${productA[j]}${productA[j+1]}` === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    arraySubtractorAtoB++
+                    break;
+                }
+                if(productA[j] === `${productB[m]}${productB[m+1]}`){
+                    matchwingWordsCountAtoB++;
+                    break;
+                }
+                //This usually catches brands that are 2 letters with an & in the middle
+                if(`${productA[j]}${productA[j+1]}${productA[j+2]}` === productB[m]){
+                    matchwingWordsCountAtoB++;
+                    arraySubtractorAtoB+=2;
+                    break;
+                }
+                if(productA[j] === `${productB[m]}${productB[m+1]}${productB[m+2]}`){
+                    matchwingWordsCountAtoB++;
+                    break;
+                }
             }
-
-            let yoyoab = getMatchPercentage(productA,productB);
-            let accuracyBtoA = getMatchPercentage(productB,productA);
-            let resultsAverage = (yoyoab + accuracyBtoA)/2
-            resultsAverage = resultsAverage.toFixed(2);
-            console.log(resultsAverage);
-            // let matchwingWordsCountAtoB = 0;
-            // let arraySubtractorAtoB = 0; //when a word gets conca and a match returns we need to subtract it from word count
-            // for(let j = 0; j < productA.length;j++){
-            //     for(let m = 0; m < productB.length; m++){
-            //         if(productA[j] === productB[m]){
-            //             matchwingWordsCountAtoB++;
-            //             break;
-            //         }
-            //         if(`${productA[j]}.` === productB[m]){
-            //             matchwingWordsCountAtoB++;
-            //             break;
-            //         }
-            //         if(`${productA[j]}${productA[j+1]}` === productB[m]){
-            //             matchwingWordsCountAtoB++;
-            //             arraySubtractorAtoB++
-            //             break;
-            //         }
-            //         if(productA[j] !== productB[m]){
-
-            //         }
-            //     }
-            // }
-            
-            // let matchwingWordsCountBtoA = 0;
-            // let arraySubtractorBtoA = 0; //when a word gets conca and a match returns we need to subtract it from word count
-            // for(let s = 0; s < productB.length;s++){
-            //     for(let f = 0; f < productA.length; f++){
-            //         if(productB[s] === productA[f]){
-            //             matchwingWordsCountBtoA++;
-            //             break;
-            //         }
-            //         if(`${productA[s]}.` === productB[f]){
-            //             matchwingWordsCountBtoA++;
-            //             break;
-            //         }
-            //         if(`${productA[s]}${productA[s+1]}` === productB[f]){
-            //             matchwingWordsCountBtoA++;
-            //             arraySubtractorBtoA++
-            //             break;
-            //         }
-                  
-
-            //     }
-            // }
-
-            // let accuracyAtoB = (matchwingWordsCountAtoB/(productA.length - arraySubtractorAtoB))*100;
-            // let accuracyBtoA = (matchwingWordsCountBtoA/(productB.length - arraySubtractorBtoA))*100;
-            // let resultsAverage = (accuracyAtoB + accuracyBtoA)/2
-
-
-            // resultsAverage = resultsAverage.toFixed(2);
-
-            productA = productA.join(" ");
-            productB = productB.join(" ");
-         
-
-             newData.push(excelData[i] = {
-                'itemNum':excelData[i]['itemNum'],
-                'UPC':excelData[i]['UPC'],
-                'drizlyName':excelData[i]['drizlyName'],
-                'ucName':excelData[i]['ucName'],
-                'category':excelData[i]['category'],
-                'ucNameClean':productA,
-                'drizlyNameClean':productB,
-                'percentageMatch':resultsAverage
-            });
-
-
-            // const accuracy = (matchwingWordsCount/productA.length)*100;
-      
-
-           // console.log(`${i} ${accuracy.toFixed(2)}% || ${productA} || ${productB}`)
         }
+        let accuracyAtoB = (matchwingWordsCountAtoB/(productA.length - arraySubtractorAtoB))*100;
+        return accuracyAtoB;
     }
 }
 
-calculateStringMatch.cleanStringMatch(excelData);
-
+calculateStringMatch.getMatchResults(excelData);
 
 // Read QC Template into a file
 const workbook = new ExcelJS.Workbook();
@@ -279,35 +233,32 @@ await workbook.xlsx.readFile(
     './excel/confidenceLevelResultsTemplate.xlsx'
 );
 const sheet = workbook.getWorksheet('Sheet1');
-
-const rows = sheet.getRows(2, newData.length);
-
-for (let i = 0; i < newData.length; i++) {
+const rows = sheet.getRows(2, matchResults.length);
+for (let i = 0; i < matchResults.length; i++) {
     const upc          = rows[i].getCell('A');
     const itemNum      = rows[i].getCell('B');
     const ucNames      = rows[i].getCell('C');
     const drizlyNames  = rows[i].getCell('D');
     const ucClean      = rows[i].getCell('E');
     const drizlyClean  = rows[i].getCell('F');
+    const ogPercentage = rows[i].getCell('G');;
     const cleanPercent = rows[i].getCell('H');
-    // const category    = rows[i].getCell('E');
+
     
+    upc.value            = matchResults[i]['UPC'];
+    itemNum.value        = matchResults[i]['itemNum'];
+    ucNames.value        = matchResults[i]['ucName'];
+    drizlyNames.value    = matchResults[i]['drizlyName'];
+    ucClean.value        = matchResults[i]['ucNameClean'];
+    drizlyClean.value    = matchResults[i]['drizlyNameClean'];
+    ogPercentage.value   = matchResults[i]['originalPercentageMatch'];
+    cleanPercent.value   = matchResults[i]['cleanPercentageMatch'];
 
-    upc.value            = newData[i]['UPC'];
-    itemNum.value        = newData[i]['itemNum'];
-    ucNames.value        = newData[i]['ucName'];
-    drizlyNames.value    = newData[i]['drizlyName'];
-    ucClean.value        = newData[i]['ucNameClean'];
-    drizlyClean.value    = newData[i]['drizlyNameClean'];
-    cleanPercent.value   = newData[i]['percentageMatch'];
-
-    // category.value    = excelData[i]['çcategory'];
 }
-
 await workbook.xlsx.writeFile(
     `./excel/confidenceLevelResults.xlsx`
 );
 
-
+console.log('script ran');
 
 
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-let string = ['goo','hi','bob','lets','build','it'];
-string = string.join("");
-let mybob = 'hibob';
+let string = ['goos','hi','bob','lets','build','it'];
+let string2 = 'goo'
+console.log(`${string[5]}${string[6]}`)
+;
+// if(string[0].slice(0, -1) === string2){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
 
-console.log(string.indexOf(mybob));
